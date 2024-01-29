232c56533ba92984d204f14ef54acc6c7fc0122c
young5lee <85347663+young5lee@users.noreply.github.com>
2024-01-30 00:07:07 +0900

    Create Define the numbers.py

---
+++ Define the numbers.py
@@ -0,0 +1,13 @@
+# Define the numbers
+num1 = 5
+num2 = 3
+
+# Multiplication
+product = num1 * num2
+
+# Addition
+sum = num1 + num2
+
+# Print results
+print("Product:", product)
+print("Sum:", sum)
