3c18dccdb11df66385f08776ddc1e727d5071a15
young5lee <85347663+young5lee@users.noreply.github.com>
2024-01-30 00:08:22 +0900

    Create convert minutes to hours.py

---
+++ convert minutes to hours.py
@@ -0,0 +1,10 @@
+# Convert Minutes to Hours
+
+# Define the number of minutes
+minutes = 150
+
+# Conversion: 1 hour = 60 minutes
+hours = minutes / 60
+
+# Print result
+print(f"{minutes} minutes is equal to {hours} hours.")††
